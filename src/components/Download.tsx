80337ef2572692edf2403afe5908eb2e1f5f476c
Dimex19 <azeezabolaji06gmail.com>
2025-03-07 12:31:05 +0100

    download component responsiveness in progress

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -5,12 +5,12 @@ const Download = () => {
   return (
     <div>
       <div className="hidden relative md:flex flex-col md:flex-row flex-wrap items-end mx-3 md:mx-[100px] mt-[63px] mb-[56px] bg-gradient-to-r from-[#ffffff] to-[#E0E1DC] ">
-      <div className=" w-[50%] h-[611px] flex items-end z-10 border border-red-600">
+      <div className=" w-[50%] h-[611px] flex items-end z-10">
         <img src="/assets/friends1.png" alt="" className="absolute bottom-0 left-0" />
         <img src="/assets/friends2.png" alt="" className="absolute bottom-0 left-4" />
       </div>
 
-      <div className="flex flex-col absolute right-0 w-[60%] h-full items-center border border-black">
+      <div className="flex flex-col absolute right-0 w-[60%] h-full items-center">
         <div className="flex items-center space-x-4 mt-[110px]">
           <p className="flex items-center text-[64px] font-bold"><img src="/assets/logo.png" alt="brand logo" className='h-[100%]'/>ing</p>
           <p className='text-[64px] font-bold'> is more</p>
@@ -39,14 +39,14 @@ const Download = () => {
     </div>
 
     {/* Mobile */}
-      <div className="md:hidden">
-        <div className="flex items-center space-x-4 mt-[110px]">
-          <p className="flex items-center text-[64px] font-bold"><img src="/assets/logo.png" alt="brand logo" className='h-[100%]'/>ing</p>
-          <p className='text-[64px] font-bold'> is more</p>
+      <div className="md:hidden bg-[#E0E1DC] rounded-[12px]">
+        <div className="flex justify-center items-center space-x-4 pt-[65px]">
+          <p className="flex items-center text-[42px] md:text-[64px] font-extrabold md:font-bold"><img src="/assets/logo.png" alt="brand logo" className='h-[32px] md:h-[100%]'/>ing</p>
+          <p className='text-[42px] md:text-[64px] font-extrabold md:font-bold'> is more</p>
         </div>
-        <p className="text-[44px] pr-9 text-black font-medium"><span className="text-[#FC8A06] underline">Personalised </span> & Instant</p>
-        <p>Download the Order.uk app for faster ordering</p>
-        <div className="flex border border-red mt-[23px]">
+        <p className="text-[32px] mb-3 md:mb-0 md:text-[44px] text-center md:text-left md:pr-9 text-black font-medium"><span className="text-[#FC8A06] underline">Personalised</span> & Instant</p>
+        <p className='text-center text-[20px] md:text-left'>Download the Order.uk app for faster ordering</p>
+        <div className="flex justify-center mt-[23px]">
           <button className="flex justify-center p-[2px] items-center  bg-black rounded-[10px] border px-4 py-2">
             <p className="text-3xl text-white "><FaApple /></p>
             <div className="ml-2">
@@ -62,10 +62,10 @@ const Download = () => {
             </div>
           </button>
         </div>
-        <div className="relative w-[100%] h-[611px] flex items-end border border-red-600">
-          <img src="/assets/friends1.png" alt="" className="absolute bottom-0 left-0" />
-          <img src="/assets/friends2.png" alt="" className="absolute bottom-0 left-4" />
-      </div>
+        <div className="relative w-[100%] -mt-[100px] h-[511px] flex justify-center items-end">
+          <img src="/assets/friends1.png" alt="" className="absolute w-[80vw] bottom-0" />
+          <img src="/assets/friends2.png" alt="" className="absolute w-[80vw] bottom-0 ml-5" />
+        </div>
       </div>
     </div>
   )
